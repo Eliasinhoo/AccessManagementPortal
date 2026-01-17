48c11895fbe0d5b14c23885605a9de3a75eea7bf
Eliasinhoo <155157087+Eliasinhoo@users.noreply.github.com>
2026-01-17 11:05:10 +0000

    Add Exception Test for License Service

--- AccessManagementPortalTests/LicenseServiceTests.cs
+++ AccessManagementPortalTests/LicenseServiceTests.cs
@@ -16,15 +16,33 @@ namespace AccessManagementPortalTests
                 .Options;
 
             using var context = new ApplicationDbContext(options);
-            context.Licenses.Add(new License { Id = 1, IsActive = false });
+            context.Licenses.Add(new License { Id = 1, IsActive = false, ApplicationUserId = "test-user-id", ProductId = 1 });
             await context.SaveChangesAsync();
 
             var service = new LicenseService(context);
 
-            await service.(1);
+            await service.ToggleLicenseAsync(1);
 
             var license = await context.Licenses.FindAsync(1);
+
             Assert.True(license!.IsActive);
         }
+
+
+        [Fact]
+        public async Task ToggleLicenseAsync_Throws_WhenLicenseNotFound()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            using var context = new ApplicationDbContext(options);
+
+            var service = new LicenseService(context);
+
+            
+            await Assert.ThrowsAsync<KeyNotFoundException>(() => service.ToggleLicenseAsync(999));
+        }
+
     }
 }
